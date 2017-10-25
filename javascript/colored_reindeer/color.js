b380c43bb307030aad8f7e71acf8381d7818011b
Jenna Solis <jennabsol@gmail.com>
2017-10-25 13:21:09 -0500

    added files

---
+++ javascript/colored_reindeer/color.js
@@ -0,0 +1,11 @@
+const coloredReindeerBuilder = function () {
+    const reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
+    // Write a for loop that looks at each reindeer
+        // Invoke factory function to create reindeer object
+
+        // Put new reindeer object in coloredReindeer array
+
+
+    // Return coloredReindeer array
+}
