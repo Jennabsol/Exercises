28c02c75ffa0a9dee0fbbc9ba15bf20ba1056dd8
Jenna Solis <jennabsol@gmail.com>
2017-11-03 12:56:35 -0500

    added

---
+++ javascript/Browser_Events/cards/car.js
@@ -0,0 +1,53 @@
+// create generator function to make a unique Id for section and button ids
+
+// store an instance of id generator in a variable
+
+// get control of dom element (button) that user will click to create a card
+
+
+/* create function to create a new card to be inserted into the DOM. Card should be created when
+ user clicks the "Create" button. This card should include a delete button. Each card created
+ should include a unique id for the entire card and for the delete button. */
+
+    // console.log(event);
+    // get control of the part of the DOM where the cards will be placed.
+    
+    // create a unique id to use in article & button tags and assign to variable
+    
+    // pull text input from the textarea DOM element that the user submitted and assign to variable
+    
+    // place the html code to create the card into the DOM using .innerHTML
+    
+    // clear the contents of the textarea after the "Create" button has been clicked and the new card has been created    
+    
+
+// create event listener to listen for the click of the "Create" button and run function to create a new card
+
+
+
+// get control of DOM parent element that user will click to delete a card
+
+// create a function to delete the card that the user clicked the "delete" button on. 
+
+    // console.log event to see what was targeted
+    
+    // console.log("btnId: ", event.target.id.split("_")[1]);
+
+    // get the ID of the event that was clicked on
+    
+    // parse out the number in the ID of the event that was clicked
+    
+
+    // check if the ID of event that was clicked starts with "button_". If so, then remove that card
+    
+        // first console log a message to see if the "if statement" is working
+        // console.log("yes it starts with button_")
+        // assign the id of the card to be deleted to a variable by using document.getElementById
+        // and the "article_" + the button Id number that was stored in a variable earlier. (btnIdNum) 
+        
+        // use .removeChild functionality to remove the card by referencing the parent container 
+        
+    }
+}
+
+// add event listener to listen for a click on the delete card button, and run the function to delete card
